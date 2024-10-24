42beef2489632f112346e87f0a80d76fcc1a1915
Ivan Ivanov <134152001+tgefog@users.noreply.github.com>
2024-10-24 21:30:32 +0300

    Initial commit

---
+++ Airport/AirportApp.Data.Models/Booking.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static AirportApp.Common.EntityValidationConstants.Booking;
+
+namespace AirportApp.Data.Models
+{
+    public class Booking
+    {
+        [Key]
+        [Comment("Unique Identifier")]
+        public Guid Id { get; set; }
+        [Required]
+        [Comment("Unique identifier of the passenger")]
+        public Guid PassengerId { get; set; }
+        [Required]
+        [ForeignKey(nameof(PassengerId))]
+        [Comment("Passenger of the booking")]
+        public Passenger Passenger { get; set; } = null!;
+        [Required]
+        [Comment("Unique identifier of the flight")]
+        public Guid FlightId { get; set; }
+        [Required]
+        [ForeignKey(nameof(FlightId))]
+        [Comment("Flight of the booking")]
+        public Flight Flight { get; set; } = null!;
+        [Required]
+        [Comment("Date of the booking")]
+        public DateTime BookingDate { get; set; }
+        [Comment("Number of seat in the booking")]
+        public string SeatNumber { get; set; } = null!;
+        [Required]
+        [MaxLength(SeatNumberMaxLength)]
+        [Comment("Class of the booking")]
+        public string Class { get; set; } = null!;
+    }
+}
---
+++ Airport/AirportApp.Data.Models/Terminal.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static AirportApp.Common.EntityValidationConstants.Terminal;
+
+namespace AirportApp.Data.Models
+{
+    public class Terminal
+    {
+        [Key]
+        [Comment("Unique identifier")]
+        public Guid Id { get; set; }
+        [Required]
+        [MaxLength(NameMaxLength)]
+        [Comment("Name of the terminal")]
+        public string Name { get; set; } = null!;
+        [Required]
+        [Comment("Capacity of the terminal")]
+        public int Capacity { get; set; }
+        public ICollection<Gate> Gates { get; set; } = new List<Gate>();
+    }
+}
